91bb1789c1c727c8231783f43a4f9ec0e8e3332d
Anas Chhilif <90978567+AnasChhilif@users.noreply.github.com>
2023-11-15 16:16:25 +0100

    trying to make gitignore work

--- Server/PolyBikingServer/PolyBikingServer/IBikingService.cs
+++ Server/PolyBikingServer/PolyBikingServer/IBikingService.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace PolyBikingServer
 {
+    /**
+     * Interface for the biking service defining the functions and their respective endpoints
+     */
     internal interface IBikingService
     {
     }
